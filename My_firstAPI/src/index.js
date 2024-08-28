564c2173e33c64dfe0f6158765c491bcf3d54ec7
MateusC2 <mateuscubeiro.educ@gmail.com>
2024-08-28 14:06:57 -0300

    Atividade

--- My_firstAPI/src/index.js
+++ My_firstAPI/src/index.js
@@ -18,10 +18,44 @@ class AppController {
 
   //Define as rotas da nossa API
   routes() {
+    const users = [];
+
+    this.express.post("/users", (req, res) => {
+      const { id, nome, email, senha } = req.body;
+      users.push({ id, nome, email, senha });
+      res.status(200).send({ message: "Usuario cadastrado com sucesso" });
+    });
+
+    this.express.post("/auth", (req, res) => {
+      const {email, senha } = req.body;
+      const login = users.find((login) => login.email == email && login.senha == senha);
+      if(login) {
+        res.status(200).send({ message: "Autenticação deu certoaaa" });
+      }
+      else{
+        res.status(404).send({ message: "Usuario não cadastrado" });
+      }
+
+
+    });
+
+
+
+    this.express.get("/users/:id", (req, res) => {
+      const { id } = req.params;
+      const user = users.find((user) => user.id == id);
+      if(user){
+        res.status(200).send(user)
+      }
+      else{
+        res.status(400).send({message: "Usuario não encontrado"})
+      }
+    });
+
     //Define uma rota GET para o caminho health
     this.express.get("/health/", (req, res) => {
-      res.send({ status: "OK", nome: "Mateus" });
-    });//Essa rota é usada para verificar se a Api esta ok
+      res.send({ status: "OK" });
+    }); //Essa rota é usada para verificar se a Api esta ok
   }
 }
 
